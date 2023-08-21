f2bdb9466711c8c6c19297c5ef2d7835e1c638ce
CarloCassini <c.cassini87@gmail.com>
2023-08-21 19:48:31 +0200

    add bonus

--- js/function.js
+++ js/function.js
@@ -1,18 +1,29 @@
 function generaGriglia(numCelle, contenitoreCelle) {
   contenitoreCelle.innerHTML = " ";
   for (let i = 1; i <= numCelle; i++) {
-    generaCella(contenitoreCelle, i);
+    generaCella(contenitoreCelle, numCelle, i);
   }
 }
 
-function generaCella(container, i) {
+function generaCella(container, difficulty, i) {
   const cella = document.createElement("div");
   cella.setAttribute("data-index", i);
   cella.classList.add("cella");
+
+  if (difficulty == 100) {
+    cella.classList.add("cella-easy");
+  } else if (difficulty == 81) {
+    cella.classList.add("cella-medium");
+  } else {
+    cella.classList.add("cella-hard");
+  }
+
   cella.innerHTML = i;
 
   cella.addEventListener("click", () => {
     cella.classList.add("cella-cliccata");
+    // il toggle per togliere il colore
+    // cella.classList.toggle("cella-cliccata");
     console.log(cella.innerHTML);
   });
   container.append(cella);
--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
-console.log("java ok");
-
-// +++
-// +++
-// +++
-
 const cellsContainer = document.getElementById("contenitore-griglia");
 const myButton = document.getElementById("genera-griglia");
-let cellsTotal = 10 * 10;
 
 myButton.addEventListener("click", function () {
+  const difficulty = document.getElementById("difficolta").value;
+  console.log(difficulty);
+  let cellsTotal;
+  if (difficulty == 1) {
+    cellsTotal = 10 * 10;
+  } else if (difficulty == 2) {
+    cellsTotal = 9 * 9;
+  } else {
+    cellsTotal = 7 * 7;
+  }
   generaGriglia(cellsTotal, cellsContainer);
 });
