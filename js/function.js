4c38961b09b47ea33ff13f73d0313f2175e17e9f
CarloCassini <c.cassini87@gmail.com>
2023-08-21 16:22:54 +0200

    add celle

--- js/function.js
+++ js/function.js
@@ -1,8 +1,16 @@
 function generaGriglia(numCelle, contenitoreCelle) {
   contenitoreCelle.innerHTML = " ";
   for (let i = 1; i <= numCelle; i++) {
-    console.log("genera-griglia");
+    generaCella(contenitoreCelle, i);
   }
 }
 
-function generaCella(container, i) {}
+function generaCella(container, i) {
+  const cella = document.createElement("div");
+  cella.setAttribute("data-index", i);
+  cella.classList.add("cella");
+  cella.innerHTML = i;
+
+  container.append(cella);
+  console.log("test");
+}
