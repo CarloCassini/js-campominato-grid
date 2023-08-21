a67d60f963d0692332c3591bfd655f5104dfe930
CarloCassini <c.cassini87@gmail.com>
2023-08-21 16:03:56 +0200

    test

--- js/function.js
+++ js/function.js
@@ -1,3 +1,8 @@
 function generaGriglia(numCelle, contenitoreCelle) {
-  console.log("genera-griglia");
+  contenitoreCelle.innerHTML = " ";
+  for (let i = 1; i <= numCelle; i++) {
+    console.log("genera-griglia");
+  }
 }
+
+function generaCella(container, i) {}
